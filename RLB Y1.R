39bff19093685420ec020e27e1a1065093bb2645
Farah Ridana <105301084+faarraahhh@users.noreply.github.com>
2026-06-29 01:30:20 +0700

    Update RLB Y1.R

--- RLB Y1.R
+++ RLB Y1.R
@@ -4,7 +4,7 @@ library(pscl)
 library(car)
 
 
-data <- read_excel("D:/Farah_kuliah/smt 7/skripsi/data.xlsx", 
+data <- read_excel("data/sakernas_2024.xlsx", 
     sheet = "Sheet5")
 View(data)
 str(data)
